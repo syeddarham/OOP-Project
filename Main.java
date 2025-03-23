13b612c0f7cbd19f19c7e750643b991948a0501e
syeddarham <k242551@nu.edu.pk>
2025-03-23 15:10:33 +0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Welcome Page");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        frame.setLayout(null);
+        JLabel welcomeLabel = new JLabel("Welcome to the OOP Project!");
+        welcomeLabel.setBounds(100, 50, 200, 30);
+        frame.add(welcomeLabel);
+        frame.setVisible(true);
+    }
+}
